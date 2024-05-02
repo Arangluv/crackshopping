202cb342bbdd1af1c314dff658913b8308e86a11
togethercodingArang <ruhunsu3@naver.com>
2024-05-02 15:54:27 +0900

    build: GlobalTheme and dark / light theme contract 작성
    프로젝트가 정해지면 dark / lighttheme의 내용을 채워야합니다

---
+++ src/app/styles/theme.css.ts
@@ -0,0 +1,57 @@
+import {
+  createGlobalTheme,
+  createTheme,
+  createThemeContract,
+} from "@vanilla-extract/css";
+
+export const global = createGlobalTheme(":root", {
+  fontSize: {
+    XLarge: "48px",
+    large: "36px",
+    medium: "28px",
+    regular: "22px",
+    middleSmall: "18px",
+    small: "16px",
+    micro: "14px",
+  },
+  fontWeight: {
+    normal: "400",
+    medium: "500",
+    large: "600",
+  },
+});
+
+export const themeColor = createThemeContract({
+  color: {
+    mainBackground: null,
+    subBackground: null,
+    mainFontColor: null,
+    subFontColor: null,
+    accentColor: null,
+    errorColor: null,
+  },
+});
+
+const darkTheme = createTheme(themeColor, {
+  color: {
+    mainBackground: "",
+    subBackground: "",
+    mainFontColor: "",
+    subFontColor: "",
+    accentColor: "",
+    errorColor: "",
+  },
+});
+
+export const lightTheme = createTheme(themeColor, {
+  color: {
+    mainBackground: "",
+    subBackground: "",
+    mainFontColor: "",
+    subFontColor: "",
+    accentColor: "",
+    errorColor: "",
+  },
+});
+
+export const vars = { ...global, ...themeColor };
