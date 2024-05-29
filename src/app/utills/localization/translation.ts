da5d40069162b1126e4e1410a1998f1fabc6d2bb
togethercodingArang <ruhunsu3@naver.com>
2024-05-29 17:54:00 +0900

    feat: 다국어 지원 기능 추가 (en / fr / it / de)

---
+++ src/app/utills/localization/translation.ts
@@ -0,0 +1,45 @@
+import { createInstance } from "i18next";
+import { initReactI18next } from "react-i18next/initReactI18next";
+import resourcesToBackend from "i18next-resources-to-backend";
+
+const i18nConfig = {
+  locales: ["en", "de", "fr", "it"],
+  defaultLocale: "en",
+};
+
+export default async function initTranslations(
+  locale: string,
+  namespace: string,
+) {
+  console.log(`locale ${locale}`);
+  console.log(`namespace ${namespace}`);
+  console.log(`@/public/assets/locales/${locale}/${namespace}.json`);
+  const i18nInstance = createInstance();
+  let resources = null;
+  i18nInstance
+    .use(initReactI18next)
+    .use(
+      resourcesToBackend(
+        () => import(`@/public/assets/locales/${locale}/${namespace}.json`),
+      ),
+    );
+
+  await i18nInstance.init(() => {
+    return {
+      lng: locale,
+      resources,
+      fallbackLng: i18nConfig.defaultLocale,
+      supportedLngs: i18nConfig.locales,
+      defaultNS: namespace,
+      fallbackNS: namespace,
+      ns: namespace,
+      preload: resources ? [] : i18nConfig.locales,
+    };
+  });
+
+  return {
+    i18n: i18nInstance,
+    resources: i18nInstance.services.resourceStore.data,
+    t: i18nInstance.t,
+  };
+}
